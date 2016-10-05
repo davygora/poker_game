a26a1680fc71df809331b2594e9bb627406a4212
davygora <davygora.e@gmail.com>
2016-10-05 16:48:54 +0300

    add pokers controller

---
+++ app/controllers/pokers_controller.rb
@@ -0,0 +1,2 @@
+class PokersController < ApplicationController
+end
---
+++ test/controllers/pokers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PokersControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
