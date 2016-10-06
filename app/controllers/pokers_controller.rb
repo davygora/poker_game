26776f011505f8098481e6cba5b6b4a8a1330fa4
davygora <davygora.e@gmail.com>
2016-10-06 12:55:51 +0300

    add subscribe method

--- app/controllers/pokers_controller.rb
+++ app/controllers/pokers_controller.rb
@@ -1,2 +1,17 @@
 class PokersController < ApplicationController
+
+  before_filter :authorize
+
+  def index
+    @unsubscribe_pokers = Poker.where("subscribe_at IS NULL")
+    @subscribe_pokers = Poker.where("subscribe_at IS NOT NULL")
+  end
+
+  def subscribe
+    if @unsubscribe_pokers.subscribed?
+      Poker.update_all(subscribe_at: Time.now, id: params[:poker_ids])
+      redirect_to pokers_path
+    else
+      redirect_to root_path, notice: 'Вы не можете подписаться на эту игру!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+
+  resources :users, :sessions
+
+  root 'pokers#index'
+
+  resources :pokers do
+    collection do
+      put 'subscribe'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
