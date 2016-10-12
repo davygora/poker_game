1512858709e078a527ac5a7e937b66cfadcc8a30
davygora <davygora.e@gmail.com>
2016-10-12 15:54:48 +0300

    increment players after subscribe

--- app/controllers/pokers_controller.rb
+++ app/controllers/pokers_controller.rb
@@ -4,12 +4,13 @@ class PokersController < ApplicationController
 
   def index
     @unsubscribe_pokers = Poker.where("subscribe_at IS NULL")
-    @subscribe_pokers = Poker.where("subscribe_at IS NOT NULL")
+    @subscribe_pokers   = Poker.where("subscribe_at IS NOT NULL")
   end
 
   def subscribe
     @pokers = Poker.where(id: params[:poker_ids])
     @pokers.update_all(subscribe_at: Time.now)
-      redirect_to pokers_path
+    @pokers.update_all("max_players = max_players + 1")
+    redirect_to pokers_path
   end
 end
